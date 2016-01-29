9e91d964671ff7f9f904f8a41356f3fe912cce56
caromeo <carmelo.romeo85@gmail.com>
2016-01-29 11:30:39 +0100

    Update footer.php

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,13 +1,11 @@
-		<div id="footer" align="center" style="z-index: 999;">
-			<p>Powered by MDSLab <br />Visit: <a href="http://smartme.unime.it/it/">SmartME</a> and <a href="http://stack4things.unime.it/">Stack4Things</a></p>
-		<p>An absolute URL</p>
-                </div>
+    <div id="footer" align="center">
+      <p>Powered by MDSLab</p>
+    </div>
 
-		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/vendor/jquery.js"></script>
-		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/foundation.min.js"></script>
-		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/foundation/foundation.reveal.js"></script>
-		<script>  $(document).foundation();  </script>
-		<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/foundation/foundation.js"></script>
-
-	</body>
+    <script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/vendor/jquery.js"></script>
+    <script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/foundation.min.js"></script>
+    <script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/foundation/foundation.reveal.js"></script>
+    <script>  $(document).foundation();  </script>
+<script src="<?= $this -> config -> site_url() ?>assets/bower_components/foundation/js/foundation/foundation.js"></script>
+  </body>
 </html>
