479ff909697442d3e88507359c38e7da2573f695
caromeo <carmelo.romeo85@gmail.com>
2016-01-29 11:38:07 +0100

    Update footer.php

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,5 +1,6 @@
 		<div id="footer" align="center" style="z-index: 999;">
 			<p>Powered by MDSLab <br />Visit: <a href="http://smartme.unime.it/it/">SmartME</a> and <a href="http://stack4things.unime.it/">Stack4Things</a></p>
+                        <!--<img src="<?= $this -> config -> site_url() .'uploads/smartme.png'  ?>" height="90" width="170" />-->
 		<p>An absolute URL</p>
                 </div>
 
